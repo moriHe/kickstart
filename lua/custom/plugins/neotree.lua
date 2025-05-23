9ad186bf200367209b3aea39b74fd6741eb1a8b8
moriHe <mor.hettich@gmail.com>
2025-05-23 12:43:14 +0200

    first commit

---
+++ lua/custom/plugins/neotree.lua
@@ -0,0 +1,22 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  keys = {
+    { '<leader>e', '<cmd>Neotree toggle<CR>', desc = 'Toggle Neo-tree File Explorer' },
+  },
+  lazy = false, -- neo-tree will lazily load itself
+  ---@module "neo-tree"
+  ---@type neotree.Config?
+  opts = {
+    window = {
+      position = 'float',
+    },
+    -- fill any relevant options here
+  },
+}
